65518bb0f204a80e4aa3afba029bc558c555cb3a
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-06 10:20:45 +0200

    changes

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+// const navSlide = () => {
+//   const menuBtn = document.querySelector('.nav__btn');
+//   const nav = document.querySelector('.nav');
+
+
+
+//   //  nav toggle
+//   menuBtn.addEventListener('click', () => {
+//     nav.classList.toggle('nav__active');
+
+//   })
+
+
+// }
+
+// navSlide();
+
